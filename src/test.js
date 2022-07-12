d1a1c91dab42d9d0185eacee7bed07c7f7c8c18a
actions-user <mail@sammorrisdesign.co.uk>
2022-07-12 08:49:06 -0400

    add check for missing publication name

--- src/test.js
+++ src/test.js
@@ -5,16 +5,22 @@ const fetchers = require('./fetchers');
   console.log("Testing a specific feed");
   console.log("This will return all articles found using a specific entry in config");
   const publication = process.env.npm_config_publication;
-  const feed = config.feeds.find(feed => feed.publication == publication);
 
-  if (feed) {
-    console.log("Found config for", publication, "and starting fetch");
-    console.time("Time to fetch");
-    const fetchedArticles = await fetchers[feed.type](feed);
-    console.log(fetchedArticles);
-    console.timeEnd("Time to fetch");
+  if (publication) {
+    const feed = config.feeds.find(feed => feed.publication == publication);
+
+    if (feed) {
+      console.log("Found config for", publication, "and starting fetch");
+      console.time("Time to fetch");
+      const fetchedArticles = await fetchers[feed.type](feed);
+      console.log(fetchedArticles);
+      console.timeEnd("Time to fetch");
+    } else {
+      console.log("Missing config for", publication);
+      console.log("Check your config.json file to corresponding publication");
+    }
   } else {
-    console.log("Missing config for", publication);
-    console.log("Check your config.json file to corresponding publication");
+    console.log("No publication supplied");
+    console.log("Please add a publication name to test against, like this: npm run test --publication=\"The New York Times\"")
   }
 })();
