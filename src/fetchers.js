eb59f933fcdfd8146828af3396206c4736aef272
sammorrisdesign <mail@sammorrisdesign.co.uk>
2022-07-21 13:20:46 -0400

    I guess we don't need this anymore

--- src/fetchers.js
+++ src/fetchers.js
@@ -48,25 +48,6 @@ const TheGuardianAPI = async(feed) => {
   }
 }
 
-const TheWashingtonPost = async(feed) => {
-  const secrets = await utils.getSecrets();
-
-  if (secrets) {
-    const response = await fetch(secrets.waPo);
-    let articles = await response.json();
-    articles = articles.filter(article => article.published == true && article.template == 'Custom template');
-    articles = articles.map(article => new Article(
-      publication = feed.publication,
-      handle = feed.handle,
-      url = "https://www.washingtonpost.com/" + article.canonical_url,
-      headline = article.headlines.basic,
-      timestamp = article.first_publish_date
-    ));
-
-    return articles;
-  }
-}
-
 const XML = async(feed) => {
   try {
     const parser = new XMLParser();
@@ -171,7 +152,6 @@ const Twitter = async(feed) => {
 module.exports = {
   NewYorkTimesAPI: NewYorkTimesAPI,
   TheGuardianAPI: TheGuardianAPI,
-  TheWashingtonPost: TheWashingtonPost,
   XML: XML,
   Twitter: Twitter
 }
